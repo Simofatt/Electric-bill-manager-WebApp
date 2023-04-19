f148b633c199ca95ca919cdd22fff5562a4e11d1
Simofatt <100781782+Simofatt@users.noreply.github.com>
2023-04-19 05:06:19 +0200

    UPDATE

--- admin/answerReclamation.php
+++ admin/answerReclamation.php
@@ -41,7 +41,7 @@ if (!isset($_SESSION['connect'])) {
                 $mail->Host = 'smtp.gmail.com';
                 $mail->SMTPAuth = true;
                 $mail->Username = 'mohamedalhabib.fatehi@etu.uae.ac.ma';
-                $mail->Password = 'med@widadi01';
+                $mail->Password = '';
                 $mail->SMTPSecure = 'tls';
                 $mail->Port = 587;
 
--- admin/test.php
+++ admin/test.php
@@ -118,7 +118,7 @@ if (isset($_POST['submit'])  && isset($_GET['idFacture'])) {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'mohamedalhabib.fatehi@etu.uae.ac.ma';
-        $mail->Password = 'med@widadi01';
+        $mail->Password = '';
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
